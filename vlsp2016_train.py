c73ebd8f1e14b61559dc64731b051d59efbebb4a
Suong <phamsuong1551997@gmail.com>
2019-05-29 00:18:51 +0700

    Add VLSP2016_SA experiment

--- vlsp2016_train.py
+++ vlsp2016_train.py
@@ -1,3 +1,4 @@
+from time import time
 import os
 import shutil
 
@@ -13,6 +14,8 @@ from sklearn.svm import SVC
 
 from text_features import Lowercase, RemoveTone, CountEmoticons
 
+start = time()
+
 model_folder = "tmp/sentiment_svm_vlsp2016"
 try:
     shutil.rmtree(model_folder)
@@ -20,6 +23,7 @@ except:
     pass
 finally:
     os.makedirs(model_folder)
+
 estimator_C = 0.375
 lower_tfidf__ngram_range = (1, 3)
 with_tone_char__ngram_range = (1, 5)
@@ -30,6 +34,7 @@ corpus: CategorizedCorpus = DataFetcher.load_corpus(NLPData.VLSP2016_SA)
 print("\n\n>>> Sample sentences")
 for s in corpus.train[:10]:
     print(s)
+
 pipeline = Pipeline(
     steps=[
         ('features', FeatureUnion([
@@ -47,6 +52,7 @@ pipeline = Pipeline(
         ('estimator', SVC(kernel='linear', C=estimator_C, class_weight=None, verbose=True))
     ]
 )
+
 print("\n\n>>> Start training")
 classifier = TextClassifier(estimator=TEXT_CLASSIFIER_ESTIMATOR.PIPELINE, pipeline=pipeline)
 model_trainer = ModelTrainer(classifier, corpus)
@@ -59,3 +65,4 @@ def macro_f1_score(y_true, y_pred):
 model_trainer.train(model_folder, scoring=macro_f1_score)
 print("\n\n>>> Finish training")
 print(f"Your model is saved in {model_folder}")
+print(f"Running in {time() - start}")
